c63719492504a5ecd0647952c3b962037c416fa6
Sherwin Dcosta <sherwin.dcosta@diggibyte.com>
2023-04-21 16:23:34 +0530

    basic structure for node js backend applciation

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to -----  application." });
+});
